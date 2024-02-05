e3d079cbdc5335dac2ed2b2b48393c2e4d00a300
Maliy Vasyl <vasil.maliy14@gmail.com>
2024-02-05 19:26:14 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,135 @@
+import os
+import logging
+import logging_config
+from multiprocessing import Pool
+from scraper_manager import OlxScraper
+from database_manager import DatabaseManager
+from notification_manager import Messenger
+from utils import BASE_DIR
+import time
+
+scraper = OlxScraper()
+db = DatabaseManager()
+
+
+def load_target_urls() -> list:
+    """
+    Fetch the list of URLs to monitor from the file 'target_urls.txt',
+    which is located in the same directory as the script.
+
+    Returns:
+        list: list of URLs from which to collect data. If the
+        file does not exist, it creates it and returns an empty list.
+
+    """
+    file_path = os.path.join(BASE_DIR, "target_urls.txt")
+    user_message = f"The file 'target_urls.txt' has been created. Add " \
+        + f"in it at least one URL to monitor for new ads. Add 1 URL per line."
+    try:
+        with open(file_path) as f:
+            target_urls = [line.strip() for line in f]
+    except FileNotFoundError:
+        logging.info(user_message)
+        open(file_path, "w").close()
+        target_urls = []
+    if not target_urls:
+        logging.info(user_message)
+    return target_urls
+
+
+def get_new_ads_urls(all_ids: list, target_url: str) -> list:
+    """
+    Returns a list of new ad URLs (not found in the database). 
+
+    Args:
+        all_urls (list): list of URLs to be matched against the database.
+
+    Returns:
+        new_urls (list): List of URLs not found in the database.
+    """
+    new_urls, new_ids = get_new_ads_urls_for_url(target_url)
+
+    new_urls_list = []
+    new_id_list = []
+
+    if new_urls:
+        for id, url in zip(new_ids, new_urls):
+            if not id in all_ids:
+                new_urls_list.append(url)
+                new_id_list.append(id)
+
+    return new_urls_list, new_id_list
+
+
+def get_new_ads_urls_for_url(target_url: str) -> list:
+    """
+    Extracts ads for a specific URL and filters out previously processed ads.
+
+    Args:
+        target_url (str): A string representing the URL for which new ads should be retrieved.
+
+    Returns:
+        List[str]: A list of URLs representing new ads retrieved from the monitored URL.
+    """
+
+    try:
+        ads_urls, new_ids = scraper.scrape_ads_urls(target_url)
+    except ValueError as error:
+        logging.error(error)
+        return []
+    return list(ads_urls), new_ids
+
+
+def main() -> None:
+    """
+    Main function. Collects and processes ads
+    and sends notifications by email and Telegram.
+    """
+
+    target_urls = load_target_urls()
+    # добавити масив при добвавленні нової силки
+    old_id_masive = [[],[],[],[],[]]
+
+    
+    while True:
+        # print(old_id_masive)
+        # print(target_urls)
+        index = 0
+
+        for target_url, ads_ids in zip(target_urls, old_id_masive):
+            # ads_urls = get_new_ads_urls_for_url(target_url)
+
+            try:
+                # Filter out the already processed ads
+                new_ads_urls, new_ids = get_new_ads_urls(ads_ids, target_url)
+            except Exception as e:
+                Messenger.send_telegram_message('', 'Failed')
+                continue
+
+            print(f"old {index} {ads_ids}")
+            print(f"new {index} {new_ads_urls}")
+            # print(f"new {index} {new_ids}")
+
+            # Process ads in parallel, for increased speed
+            # with Pool(10) as pool:
+            #     new_ads = pool.map(scraper.get_ad_data, new_ads_urls)
+            # new_ads = list(filter(None, new_ads))
+
+
+            if new_ads_urls:
+                 for ad in new_ads_urls:
+                     message_subject, message_body = Messenger.generate_email_content(
+                         target_url, [ad])
+                #     Messenger.send_email_message(message_subject, message_body)
+                     Messenger.send_telegram_message('', message_body)
+
+            # Add the processed ads to database
+
+            old_id_masive[index].extend(new_ids)
+            index = index + 1
+
+            time.sleep(2)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scraper_manager.py
@@ -0,0 +1,249 @@
+import re
+import requests
+import logging
+import logging_config
+from multiprocessing import Pool
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup, ResultSet, Tag
+from utils import get_header
+from datetime import datetime, timedelta
+import pytz
+
+class OlxScraper:
+    """Class used to scrape data from OLX Romania."""
+
+    def __init__(self):
+        self.headers = get_header()
+        self.netloc = "www.olx.ua"
+        self.schema = "https"
+        self.current_page = 1
+        self.last_page = 1
+
+    def parse_content(self, target_url: str) -> BeautifulSoup:
+        """
+        Parse content from a given URL.
+
+        Args:
+            target_url (str): A string representing the URL to be processed.
+
+        Returns:
+            BeautifulSoup: An object representing the processed content,
+            or None in case of error.
+        """
+        try:
+            r = requests.get(target_url, headers=self.headers, timeout=32)
+            r.raise_for_status()
+        except requests.exceptions.RequestException as error:
+            logging.error(f"Connection error: {error}")
+        else:
+            parsed_content = BeautifulSoup(r.text, "html.parser")
+            return parsed_content
+
+    def get_ads(self, parsed_content: BeautifulSoup):
+        """
+        Returns all ads found on the parsed web page.
+
+        Args:
+            parsed_content (BeautifulSoup): a BeautifulSoup object created as
+            a result of parsing the web page.
+
+        Returns:
+            ResultSet[Tag]: A ResultSet containing all HTML tags that contain ads.
+        """
+        if parsed_content is None:
+            return None
+        ads = parsed_content.select("div.css-1sw7q4x:not([data-testid=\"adCard-featured\"])")
+        ads_list = []
+
+        for ad in ads:
+            p_element = ad.find('p', {'data-testid': 'location-date'})
+            if p_element and 'Сьогодні' in p_element.text:
+                # ads_list.append(ad)
+
+                text_content = p_element.text.strip()
+
+                # Виділяємо годину і хвилину з тексту
+                time_str = text_content.split('о')[-1].strip()
+                event_time = datetime.strptime(time_str, '%H:%M').time()
+                delta = timedelta(hours=2)
+
+                # Добавляем timedelta к объекту времени
+                event_time = (datetime.combine(datetime.min, event_time) + delta).time()
+                # Отримуємо поточний час в Києві
+                current_time_kiev = datetime.now(pytz.timezone('Europe/Kiev')).time()
+
+                # Порівнюємо часи
+                time_difference = datetime.combine(datetime.min, event_time) - datetime.combine(datetime.min, current_time_kiev)
+                # print(event_time)
+                if abs(time_difference) <= timedelta(minutes=30):
+                    print(event_time)
+                    ads_list.append(ad)
+
+        return ads_list
+
+    def get_last_page(self, parsed_content: BeautifulSoup) -> int:
+        """
+        Returns the number of the last page available for processing.
+
+        Args:
+            parsed_content (BeautifulSoup): a BeautifulSoup object created
+            as a result of parsing the web page.
+
+        Returns:
+            int: The number of the last page available for parsing. If
+            there is no paging or the parsed object is None, it will return None.
+        """
+        if parsed_content is not None:
+            pagination_ul = parsed_content.find("ul", class_="pagination-list")
+            if pagination_ul is not None:
+                pages = pagination_ul.find_all("li", class_="pagination-item")
+                if pages:
+                    return int(pages[-1].text)
+        return None
+
+    def scrape_ads_urls(self, target_url: str) -> list:
+        """
+        Scrapes the URLs of all valid ads present on an OLX page. Search all relevant
+        URLs of the ads and adds them to a set. Parses all pages, from first to last.
+
+        Args:
+            target_url (str): URL of the OLX page to start the search from.
+
+        Returns:
+            list: a list of relevant URLs of the ads found on the page.
+
+        Raises:
+            ValueError: If the URL is invalid or does not belong to the specified domain.
+        """
+        ads_links = []
+        ads_id = []
+
+        if self.netloc != urlparse(target_url).netloc:
+            raise ValueError(
+                f"Bad URL! OLXRadar is configured to process {self.netloc} links only.")
+        while True:
+            url = f"{target_url}"
+            parsed_content = self.parse_content(url)
+            # self.last_page = self.get_last_page(parsed_content)
+            self.last_page = 1
+            ads = self.get_ads(parsed_content)
+            if ads is None:
+                return ads_links
+            for ad in ads:
+                link = ad.find("a", class_="css-rc5s2u")
+
+                if link is not None and link.has_attr("href"):
+                    link_href = link["href"]
+                    if not self.is_internal_url(link_href, self.netloc):
+                        continue
+                    if not self.is_relevant_url(link_href):
+                        continue
+                    if self.is_relative_url(link_href):
+                        link_href = f"{self.schema}://{self.netloc}{link_href}"
+                    ads_links.append(link_href)
+                    # добавити id оголошення
+                    ads_id.append(ad.get('id'))
+            if self.last_page is None or self.current_page >= self.last_page:
+                break
+            self.current_page += 1
+        return ads_links, ads_id
+
+    def is_relevant_url(self, url: str) -> bool:
+        """
+        Determines whether a particular URL is relevant by analyzing the query segment it contains.
+
+        Args:
+            url (str): A string representing the URL whose relevance is to be checked.
+
+        Returns:
+            bool: True if the URL is relevant, False if not.
+
+        The query (or search) segments, such as "?reason=extended-region", show that the ad
+        is added to the search results list by OLX when there are not enough ads
+        available for the user's region. Therefore, such a URL is not useful
+        (relevant) for monitoring.
+        """
+        segments = urlparse(url)
+        if segments.query != "":
+            return False
+        return True
+
+    def is_internal_url(self, url: str, domain: str) -> bool:
+        """
+        Checks if the URL has the same domain as the page it was taken from.
+
+        Args:
+            url (str): the URL to check.
+            domain (str): Domain of the current page.
+
+        Returns:
+            bool: True if the URL is an internal link, False otherwise.
+        """
+        # URL starts with "/"
+        if self.is_relative_url(url):
+            return True
+        parsed_url = urlparse(url)
+        if parsed_url.netloc == domain:
+            return True
+        return False
+
+    def is_relative_url(self, url: str) -> bool:
+        """
+        Check if the given url is relative or absolute.
+
+        Args:
+            url (str): url to check.
+
+        Returns:
+            True if the url is relative, otherwise False.
+        """
+
+        parsed_url = urlparse(url)
+        if not parsed_url.netloc:
+            return True
+        if re.search(r"^\/[\w.\-\/]+", url):
+            return True
+        return False
+
+    def get_ad_data(self, ad_url: str):
+        """
+        Extracts data from the HTML page of the ad.
+
+        Args:
+            ad_url (str): the URL of the ad.
+
+        Returns:
+            dict or None: A dictionary containing the scraped ad data
+            or None if the required information is missing.
+        """
+        logging.info(f"Processing {ad_url}")
+        content = self.parse_content(ad_url)
+
+        if content is None:
+            return None
+
+        title = None
+        if content.find("h1", class_="css-1soizd2"):
+            title = content.find(
+                "h1", class_="css-1soizd2").get_text(strip=True)
+        price = None
+        if content.find("h3", class_="css-ddweki"):
+            price = content.find(
+                "h3", class_="css-ddweki").get_text(strip=True)
+        description = None
+        if content.find("div", class_="css-bgzo2k"):
+            description = content.find(
+                "div", class_="css-bgzo2k").get_text(strip=True, separator="\n")
+        seller = None
+        if content.find("h4", class_="css-1lcz6o7"):
+            seller = content.find(
+                "h4", class_="css-1lcz6o7").get_text(strip=True)
+        if any(item is None for item in [title, price, description]):
+            return None
+        ad_data = {
+            "title": title,
+            "price": price,
+            "url": ad_url,
+            "description": description
+        }
+        return ad_data
