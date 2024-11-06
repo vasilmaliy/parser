6f6c199966321f77f5844b800aee7744380be190
Maliy Vasyl <vasil.maliy14@gmail.com>
2024-11-06 21:12:29 +0200

    change class name

--- scraper_manager.py
+++ scraper_manager.py
@@ -52,7 +52,8 @@ class OlxScraper:
         """
         if parsed_content is None:
             return None
-        ads = parsed_content.select("div.css-1sw7q4x:not([data-testid=\"adCard-featured\"])")
+        #поміняти на клас який є елементом оголошення
+        ads = parsed_content.select("div.css-l9drzq:not([data-testid=\"adCard-featured\"])")
         ads_list = []
 
         for ad in ads:
